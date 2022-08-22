6078308e65e8fd3a200e1dc68a6d59d185c24eab
AnaxGotJuice <64612032+AnaxGotJuice@users.noreply.github.com>
2022-08-22 01:17:59 -0700

    removed unused fonts from import

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,10 @@ export default function Home() {
         <meta name="description" content="video streaming frontend" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigini />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Aboreto&family=Alumni+Sans+Pinstripe&family=Quicksand:wght@300;400;500;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Aboreto&display=swap"
           rel="stylesheet"
-          i
         />
       </Head>
       <Header />
