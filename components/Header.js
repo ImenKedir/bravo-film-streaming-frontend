201f16c695116738b8ee449ee4ac6fd9eec3ef9c
AnaxGotJuice <64612032+AnaxGotJuice@users.noreply.github.com>
2022-08-25 01:17:40 -0700

    new icons and working nav links on header

--- components/Header.js
+++ components/Header.js
@@ -1,27 +1,34 @@
-import {
-  BadgeCheckIcon,
-  CollectionIcon,
-  HomeIcon,
-  LightningBoltIcon,
-  SearchIcon,
-  UserIcon,
-} from "@heroicons/react/outline";
 import HeaderItem from "./HeaderItem";
+import Link from "next/link";
+import { HiHome, HiSearch, HiCollection} from "react-icons/hi";
 
 const Header = () => {
   return (
     <header className="mx-auto p-8 max-w-[1920px] flex flex-col md:flex-row md:justify-between items-center">
       <div className="p-4 text-center">
-      <h1 className="text-6xl font-Aboreto font-bold text-bravo-dark">Bravo</h1>
-      <p className="p-2 text-sm font-Oswald font-light text-bravo-accent">Film Streaming Platform</p>
+        <h1 className="text-6xl font-Aboreto font-bold text-bravo-dark">
+          Bravo
+        </h1>
+        <p className="p-2 text-sm font-Oswald font-light text-bravo-accent">
+          Film Streaming Platform
+        </p>
       </div>
       <div className="py-2 flex flex-gow justify-evenly">
-        <HeaderItem title="HOME" Icon={HomeIcon} />
-        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
-        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
-        <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
-        <HeaderItem title="SEARCH" Icon={SearchIcon} />
-        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+        <Link href="/">
+          <a>
+            <HeaderItem title="HOME" Icon={HiHome} />
+          </a>
+        </Link>
+        <Link href="/explore">
+          <a>
+            <HeaderItem title="EXPLORE" Icon={HiCollection} />
+          </a>
+        </Link>
+        <Link href="/search">
+          <a>
+            <HeaderItem title="EXPLORE" Icon={HiSearch} />
+          </a>
+        </Link>
       </div>
     </header>
   );
--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const HeaderItem = ({title, Icon}) => {
   return (
-    <div className='group w-10 lg:w-12 mx-2 md:mx-4 lg:mx-6 flex flex-col items-center cursor-pointer group text-bravo-dark hover:text-bravo-accent'>
-        <Icon className="group-hover:animate-bounce"/>
+    <div className='group w-10 lg:w-12 mx-4 md:mx-5 lg:mx-6 flex flex-col items-center cursor-pointer group text-bravo-dark hover:text-bravo-accent'>
+        <Icon className="w-full h-full group-hover:animate-bounce"/>
         <p className='text-xs lg:text-base tracking-widest opacity-0 group-hover:opacity-100'>{title}</p>
     </div>
   )
