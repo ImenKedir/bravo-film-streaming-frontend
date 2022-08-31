7e579f2fc7dc22de9783ccacd8d2eb5f26771fd4
AnaxGotJuice <64612032+AnaxGotJuice@users.noreply.github.com>
2022-08-31 17:04:24 -0500

    fixed requests

--- utils/requests.js
+++ utils/requests.js
@@ -11,34 +11,38 @@ export default {
   },
   fetchActionMovies: {
     title: "Action",
-    url: `/action/all/week?api_key=${API_KEY}&language=en-US`,
+    url: `/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=28`,
   },
   fetchComedyMovies: {
     title: "Comedy",
-    url: `/comedy/all/week?api_key=${API_KEY}&language=en-US`,
+    url: `/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=35`,
   },
   fetchHorrorMovies: {
     title: "Horror",
-    url: `/horror/all/week?api_key=${API_KEY}&language=en-US`,
+    url: `/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=27`,
   },
   fetchRomanceMovies: {
     title: "Romance",
-    url: `/romance/all/week?api_key=${API_KEY}&language=en-US`,
+    url: `/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=10749`,
   },
   fetchMysteryMovies: {
     title: "Mystery",
-    url: `/mystery/all/week?api_key=${API_KEY}&language=en-US`,
+    url: `/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=9648`,
   },
   fetchSciFiMovies: {
     title: "Sci-Fi",
-    url: `/scifi/all/week?api_key=${API_KEY}&language=en-US`,
+    url: `/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=878`,
   },
   fetchWesternMovies: {
     title: "Western",
-    url: `/western/all/week?api_key=${API_KEY}&language=en-US`,
+    url: `/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=37`,
   },
-  Animation: {
+  fetchAnimation: {
     title: "Animation",
-    url: `/animaiton/all/week?api_key=${API_KEY}&language=en-US`,
+    url: `/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=16`,
+  },
+  fetchTV: {
+    title: "Animation",
+    url: `/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=10770`,
   },
 };
