b4b281e4f6e4bf7b061fc14e0e22191a54afa025
AnaxGotJuice <64612032+AnaxGotJuice@users.noreply.github.com>
2022-08-31 20:12:08 -0500

    delete animate css import

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import Nav from "../components/Nav";
 import Results from "../components/Results";
 import requests from "../utils/requests";
-import 'animate.css';
 
 export default function Home({ films }) {
   return (
